4a719febe7c45084ccc448b30e3d740a63511e57
camshaft <bytheway.cameron@gmail.com>
2015-01-24 09:03:06 -0700

    release 0.3.0

--- test/client.test.js
+++ test/client.test.js
@@ -5,10 +5,12 @@
 var annex = require('..');
 var marshal = require('annex-marshal-msgpack-node');
 
-var client = module.exports = annex.Client('wss://poe-math-service.herokuapp.com', {marshal: marshal});
+var client = module.exports = annex.Client('ws://localhost:3000', {marshal: marshal});
 
 client.add = function() {
   client.call('math', 'add', Array.prototype.slice.call(arguments), function(err, res) {
     console.log(res);
   });
 };
+
+client.add(1,2,3,4);
--- test/server.test.js
+++ test/server.test.js
@@ -13,6 +13,7 @@ var wss = new annex.Server({server: server, marshal: marshal});
 
 wss.listen(function(req, res) {
   if (req.module === 'math' && req.method === 'random') return res.send(Math.random());
+  if (req.module === 'math' && req.method === 'add') return res.send(req.params.reduce(function(acc, n) {return acc + n}, 0))
   if (req.module === 'string' && req.method === 'concat') return res.send(req.params.reduce(function(acc, str) { return acc + str; }, ''));
   res.error('Undefined ' + req.module + ':' + req.method);
 });
